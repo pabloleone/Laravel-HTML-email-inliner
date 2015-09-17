ed864e57efbed4df0b57abbd54c1c63824152190
Pablo Ezequiel Leone Signetti <pablo.signetti@castletrust.co.uk>
2015-09-17 16:16:27 +0100

    psr2

--- src/config/config.php
+++ src/config/config.php
@@ -2,19 +2,19 @@
 
 return [
 
-	'cache_path' => storage_path().'/framework/cache/',
+    'cache_path' => storage_path().'/framework/cache/',
 
     'options' => [
-        'css_to_attributes'     => true,
-        'include_link_tags'     => true,
-        'include_style_tags'    => true,
-        'input_encoding'        => 'ASCII-8BIT',
-        'preserve_reset'        => true,
-        'preserve_styles'       => false,
-        'remove_classes'        => false,
-        'remove_comments'       => true,
-        'remove_ids'            => false,
-        'remove_scripts'        => true,
+        'css_to_attributes' => true,
+        'include_link_tags' => true,
+        'include_style_tags' => true,
+        'input_encoding' => 'ASCII-8BIT',
+        'preserve_reset' => true,
+        'preserve_styles' => false,
+        'remove_classes' => false,
+        'remove_comments' => true,
+        'remove_ids' => false,
+        'remove_scripts' => true,
         'replace_html_entities' => false,
     ],
 
